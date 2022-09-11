39e6da1f1a330f020d0b2860c268a4733a1c4422
Lukasz Karpik <lkarpik@wp.pl>
2022-09-11 20:11:40 +0200

    Aded chart js bundle

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,74 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ChartBuilderInterface  $chartBuilder): Response
     {
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => -50,
+                    'suggestedMax' => 50,
+                ],
+            ],
+        ]);
+
+        $chart2 = $chartBuilder->createChart(Chart::TYPE_BAR);
+
+        $chart2->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My second dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                    'borderWidth' => 2,
+                    'borderRadius' => 5
+                ],
+                [
+                    'label' => 'My third dataset',
+                    'backgroundColor' => 'rgb(55, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [3, 11, 45, 0, 18, 30, 33],
+                    'borderWidth' => 2,
+                    'borderRadius' => 15
+                ],
+            ],
+        ]);
+
+        $chart2->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 50,
+                ],
+            ],
+        ]);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'chart' => $chart,
+            'chart2' => $chart2
         ]);
     }
 }
